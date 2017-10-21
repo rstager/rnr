5818acd091353cef0bbda52059aae31db57e7292
rstager <roger@stager.us>
2017-10-20 17:27:17 -0700

    moved hello_world into gym

--- rnr/gym.py
+++ rnr/gym.py
@@ -11,6 +11,17 @@ gym.envs.register(
             }
 )
 gym.envs.register(
+    id='NServoArm-v1',
+    entry_point='rnr.nservoarm:NServoArmEnv',
+    max_episode_steps=500,
+    kwargs={#'ngoals': 10,  # 'image_goal':(160,320)
+            # 'goals': [(1, 1)],
+            # 'initial_angles': [[0, 0]],
+            'deadband_reward': False,
+            'deadband_stop':True,
+            }
+)
+gym.envs.register(
     id='NServoArmImageOnly-v0',
     entry_point='rnr.nservoarm:NServoArmEnv',
     max_episode_steps=500,
@@ -33,3 +44,22 @@ gym.envs.register(
             'image_goal':(160,320),
             }
 )
+gym.envs.register(
+    id='Slider-v0',
+    entry_point='rnr.slider:SliderEnv',
+    max_episode_steps=500,
+    kwargs={}
+)
+
+def hello_world(envname):
+    env = gym.make(envname)
+    for i_episode in range(200):
+        observation = env.reset()
+        for t in range(1000):
+            env.render()
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            print(reward,observation)
+            if done:
+                print("Episode finished after {} timesteps".format(t+1))
+                break
