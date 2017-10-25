d1c8fe5e3cef4b3e006173dda50325e2599769d8
rstager <roger@stager.us>
2017-10-25 10:48:29 -0700

    initial

---
+++ rnr/mgu.py
@@ -0,0 +1,268 @@
+from __future__ import absolute_import
+import numpy as np
+
+from keras import backend as K
+from keras import activations
+from keras import initializers
+from keras import regularizers
+from keras import constraints
+from keras.engine import Layer
+from keras.engine import InputSpec
+from keras.legacy import interfaces
+from keras.layers.recurrent import Recurrent,_time_distributed_dense
+
+
+class MGU(Recurrent):
+    """Minimal Gated Recurrent Unit - Guo-Bing Zhou et al. 2016.
+
+    # Arguments
+        units: Positive integer, dimensionality of the output space.
+        activation: Activation function to use
+            (see [activations](../activations.md)).
+            If you pass None, no activation is applied
+            (ie. "linear" activation: `a(x) = x`).
+        recurrent_activation: Activation function to use
+            for the recurrent step
+            (see [activations](../activations.md)).
+        use_bias: Boolean, whether the layer uses a bias vector.
+        kernel_initializer: Initializer for the `kernel` weights matrix,
+            used for the linear transformation of the inputs.
+            (see [initializers](../initializers.md)).
+        recurrent_initializer: Initializer for the `recurrent_kernel`
+            weights matrix,
+            used for the linear transformation of the recurrent state.
+            (see [initializers](../initializers.md)).
+        bias_initializer: Initializer for the bias vector
+            (see [initializers](../initializers.md)).
+        kernel_regularizer: Regularizer function applied to
+            the `kernel` weights matrix
+            (see [regularizer](../regularizers.md)).
+        recurrent_regularizer: Regularizer function applied to
+            the `recurrent_kernel` weights matrix
+            (see [regularizer](../regularizers.md)).
+        bias_regularizer: Regularizer function applied to the bias vector
+            (see [regularizer](../regularizers.md)).
+        activity_regularizer: Regularizer function applied to
+            the output of the layer (its "activation").
+            (see [regularizer](../regularizers.md)).
+        kernel_constraint: Constraint function applied to
+            the `kernel` weights matrix
+            (see [constraints](../constraints.md)).
+        recurrent_constraint: Constraint function applied to
+            the `recurrent_kernel` weights matrix
+            (see [constraints](../constraints.md)).
+        bias_constraint: Constraint function applied to the bias vector
+            (see [constraints](../constraints.md)).
+        dropout: Float between 0 and 1.
+            Fraction of the units to drop for
+            the linear transformation of the inputs.
+        recurrent_dropout: Float between 0 and 1.
+            Fraction of the units to drop for
+            the linear transformation of the recurrent state.
+
+    # References
+        - [On the Properties of Neural Machine Translation: Encoder-Decoder Approaches](https://arxiv.org/abs/1409.1259)
+        - [Empirical Evaluation of Gated Recurrent Neural Networks on Sequence Modeling](http://arxiv.org/abs/1412.3555v1)
+        - [A Theoretically Grounded Application of Dropout in Recurrent Neural Networks](http://arxiv.org/abs/1512.05287)
+    """
+
+    @interfaces.legacy_recurrent_support
+    def __init__(self, units,
+                 activation='tanh',
+                 recurrent_activation='hard_sigmoid',
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 recurrent_initializer='orthogonal',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 recurrent_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 recurrent_constraint=None,
+                 bias_constraint=None,
+                 dropout=0.,
+                 recurrent_dropout=0.,
+                 **kwargs):
+        super(MGU, self).__init__(**kwargs)
+        self.units = units
+        self.activation = activations.get(activation)
+        self.recurrent_activation = activations.get(recurrent_activation)
+        self.use_bias = use_bias
+
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.recurrent_initializer = initializers.get(recurrent_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.recurrent_regularizer = regularizers.get(recurrent_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.recurrent_constraint = constraints.get(recurrent_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+
+        self.dropout = min(1., max(0., dropout))
+        self.recurrent_dropout = min(1., max(0., recurrent_dropout))
+        self.state_spec = InputSpec(shape=(None, self.units))
+
+    def build(self, input_shape):
+        if isinstance(input_shape, list):
+            input_shape = input_shape[0]
+
+        batch_size = input_shape[0] if self.stateful else None
+        self.input_dim = input_shape[2]
+        self.input_spec[0] = InputSpec(shape=(batch_size, None, self.input_dim))
+
+        self.states = [None]
+        if self.stateful:
+            self.reset_states()
+
+        self.kernel = self.add_weight(shape=(self.input_dim, self.units * 3),
+                                      name='kernel',
+                                      initializer=self.kernel_initializer,
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        self.recurrent_kernel = self.add_weight(
+            shape=(self.units, self.units * 3),
+            name='recurrent_kernel',
+            initializer=self.recurrent_initializer,
+            regularizer=self.recurrent_regularizer,
+            constraint=self.recurrent_constraint)
+
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.units * 3,),
+                                        name='bias',
+                                        initializer=self.bias_initializer,
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+
+        self.kernel_f = self.kernel[:, :self.units]
+        self.recurrent_kernel_f = self.recurrent_kernel[:, :self.units]
+
+        self.kernel_h = self.kernel[:, self.units * 2:]
+        self.recurrent_kernel_h = self.recurrent_kernel[:, self.units * 2:]
+
+        if self.use_bias:
+            self.bias_f = self.bias[:self.units]
+            self.bias_h = self.bias[self.units * 2:]
+        else:
+            self.bias_f = None
+            self.bias_h = None
+        self.built = True
+
+    def preprocess_input(self, inputs, training=None):
+        if self.implementation == 0:
+            input_shape = K.int_shape(inputs)
+            input_dim = input_shape[2]
+            timesteps = input_shape[1]
+
+            x_f = _time_distributed_dense(inputs, self.kernel_f, self.bias_f,
+                                          self.dropout, input_dim, self.units,
+                                          timesteps, training=training)
+
+            x_h = _time_distributed_dense(inputs, self.kernel_h, self.bias_h,
+                                          self.dropout, input_dim, self.units,
+                                          timesteps, training=training)
+            return K.concatenate([x_f, x_h], axis=2)
+        else:
+            return inputs
+
+    def get_constants(self, inputs, training=None):
+        constants = []
+        if self.implementation != 0 and 0 < self.dropout < 1:
+            input_shape = K.int_shape(inputs)
+            input_dim = input_shape[-1]
+            ones = K.ones_like(K.reshape(inputs[:, 0, 0], (-1, 1)))
+            ones = K.tile(ones, (1, int(input_dim)))
+
+            def dropped_inputs():
+                return K.dropout(ones, self.dropout)
+
+            dp_mask = [K.in_train_phase(dropped_inputs,
+                                        ones,
+                                        training=training) for _ in range(3)]
+            constants.append(dp_mask)
+        else:
+            constants.append([K.cast_to_floatx(1.) for _ in range(3)])
+
+        if 0 < self.recurrent_dropout < 1:
+            ones = K.ones_like(K.reshape(inputs[:, 0, 0], (-1, 1)))
+            ones = K.tile(ones, (1, self.units))
+
+            def dropped_inputs():
+                return K.dropout(ones, self.recurrent_dropout)
+            rec_dp_mask = [K.in_train_phase(dropped_inputs,
+                                            ones,
+                                            training=training) for _ in range(3)]
+            constants.append(rec_dp_mask)
+        else:
+            constants.append([K.cast_to_floatx(1.) for _ in range(3)])
+        return constants
+
+    def step(self, inputs, states):
+        h_tm1 = states[0]  # previous memory
+        dp_mask = states[1]  # dropout matrices for recurrent units
+        rec_dp_mask = states[2]
+
+        if self.implementation == 2:
+            matrix_x = K.dot(inputs * dp_mask[0], self.kernel)
+            if self.use_bias:
+                matrix_x = K.bias_add(matrix_x, self.bias)
+            matrix_inner = K.dot(h_tm1 * rec_dp_mask[0],
+                                 self.recurrent_kernel[:, :2 * self.units])
+
+            x_f = matrix_x[:, :self.units]
+
+            recurrent_f = matrix_inner[:, :self.units]
+
+            f = self.recurrent_activation(x_f + recurrent_f)
+
+            x_h = matrix_x[:, 2 * self.units:]
+            recurrent_h = K.dot(f * h_tm1 * rec_dp_mask[0],
+                                self.recurrent_kernel[:, 2 * self.units:])
+            hh = self.activation(x_h + recurrent_h)
+        else:
+            if self.implementation == 0:
+                x_f = inputs[:, :self.units]
+                x_h = inputs[:, 2 * self.units:]
+            elif self.implementation == 1:
+                x_f = K.dot(inputs * dp_mask[0], self.kernel_f)
+                x_h = K.dot(inputs * dp_mask[2], self.kernel_h)
+                if self.use_bias:
+                    x_f = K.bias_add(x_f, self.bias_f)
+                    x_h = K.bias_add(x_h, self.bias_h)
+            else:
+                raise ValueError('Unknown `implementation` mode.')
+            f = self.recurrent_activation(x_f + K.dot(h_tm1 * rec_dp_mask[0],
+                                                      self.recurrent_kernel_f))
+
+            hh = self.activation(x_h + K.dot(f * h_tm1 * rec_dp_mask[2],
+                                             self.recurrent_kernel_h))
+        h = f * h_tm1 + (1 - f) * hh
+        if 0 < self.dropout + self.recurrent_dropout:
+            h._uses_learning_phase = True
+        return h, [h]
+
+    def get_config(self):
+        config = {'units': self.units,
+                  'activation': activations.serialize(self.activation),
+                  'recurrent_activation': activations.serialize(self.recurrent_activation),
+                  'use_bias': self.use_bias,
+                  'kernel_initializer': initializers.serialize(self.kernel_initializer),
+                  'recurrent_initializer': initializers.serialize(self.recurrent_initializer),
+                  'bias_initializer': initializers.serialize(self.bias_initializer),
+                  'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+                  'recurrent_regularizer': regularizers.serialize(self.recurrent_regularizer),
+                  'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+                  'activity_regularizer': regularizers.serialize(self.activity_regularizer),
+                  'kernel_constraint': constraints.serialize(self.kernel_constraint),
+                  'recurrent_constraint': constraints.serialize(self.recurrent_constraint),
+                  'bias_constraint': constraints.serialize(self.bias_constraint),
+                  'dropout': self.dropout,
+                  'recurrent_dropout': self.recurrent_dropout}
+        base_config = super(MGU, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
