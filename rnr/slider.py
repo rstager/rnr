d661833be0cb4eea0121882dfdd5c211060e7d37
rstager <roger@stager.us>
2017-10-25 10:48:06 -0700

    Put the puck on top

--- rnr/slider.py
+++ rnr/slider.py
@@ -36,13 +36,14 @@ class SliderEnv(gym.Env):
         self.v+=u[0]*self.dt
         self.x += self.v*self.dt
         error=abs(self.x-self.goalx)
-        reward=-error-abs(self.v)
+        reward=-error**2-self.v**2
         self.done= (error<self.deadband and abs(self.v)<self.vdeadband) or abs(self.x)>self.maxx
         return self._get_obs(), reward, self.done, {}
 
 
     def _reset(self):
         self.goalx=random.uniform(-self.maxx,self.maxx)
+        self.goalx=0.0
         self.x=random.uniform(-self.maxx,self.maxx)
         self.v=0
         return self._get_obs()
@@ -64,20 +65,23 @@ class SliderEnv(gym.Env):
             sz=self
             self.viewer.set_bounds(-1.1 * self.maxx, 1.1 * self.maxx, -1.0, 1.0)
 
-            puck = rendering.make_circle(.1)
+            goal = rendering.make_circle(.05)
+            goal.set_color(1, 0, 0)
+            self.goal_transform = rendering.Transform()
+            goal.add_attr(self.goal_transform)
+            self.viewer.add_geom(goal)
+
+            puck = rendering.make_circle(.025)
             puck.set_color(0,0,0)
             self.puck_transform=rendering.Transform()
             puck.add_attr(self.puck_transform)
             self.viewer.add_geom(puck)
 
-            goal = rendering.make_circle(.05)
-            goal.set_color(1,0,0)
-            self.goal_transform=rendering.Transform()
-            goal.add_attr(self.goal_transform)
-            self.viewer.add_geom(goal)
 
-        self.puck_transform.set_translation(self.x,0)
+
         self.goal_transform.set_translation(self.goalx,0)
+        self.puck_transform.set_translation(self.x,0)
+
         return self.viewer.render(return_rgb_array = mode=='rgb_array')
 
     def get_state(self):
