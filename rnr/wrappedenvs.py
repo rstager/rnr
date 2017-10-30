2243be1f2d4dc96a5d48988440db2eaf98458f37
rstager <roger@stager.us>
2017-10-29 22:15:54 -0700

    added mountaincart

--- rnr/wrappedenvs.py
+++ rnr/wrappedenvs.py
@@ -1,4 +1,5 @@
 from gym.envs.classic_control import PendulumEnv
+from gym.envs.classic_control import Continuous_MountainCarEnv
 
 class RestartablePendulumEnv(PendulumEnv):
     def __init__(self,*args,**kwargs):
@@ -8,3 +9,14 @@ class RestartablePendulumEnv(PendulumEnv):
         return (self.state,self.last_u)
     def set_state(self,state):
         self.state,self.last_u = state
+        return self._get_obs()
+
+class RestartableContinuousMountainCartEnv(Continuous_MountainCarEnv):
+    def __init__(self,*args,**kwargs):
+        super(RestartableContinuousMountainCartEnv,self).__init__()
+
+    def get_state(self):
+        return (self.state)
+    def set_state(self,state):
+        self.state = state
+        return self._get_obs()
