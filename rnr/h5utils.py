d866a46fc245436e556834e715e029bceeabee4c
rstager <roger@stager.us>
2018-07-27 11:26:24 -0700

    Set default as non-batch data

--- rnr/h5utils.py
+++ rnr/h5utils.py
@@ -17,7 +17,7 @@ class h5record(object):
         self.exclude=exclude
         self.datasets={}
 
-    def __call__(self,path,fields=None):
+    def __call__(self,path,fields=None,batch=False):
         '''
         Record data from dictionary. Each value will be appended to a dataset by the key.
         If the value is a numpy array or a python list, it will be appended as a row in the dataset by the
@@ -29,37 +29,42 @@ class h5record(object):
         if fields is None:
             fields=path.keys()
         for name in fields:
-            data = path.get(name,None)
-            #print("name {} len {}".format(name,len(data)))
-            if name in self.exclude:
-                continue
-            if len(data)==0:continue
-            if not name in self.datasets:
-                assert self.idx==0,'{} data set must appear in first call to record {} '.format(name,self.idx)
-                if  name in self.h5file.keys():
-                    del self.h5file[name]
-                if hasattr(data[0],'dtype'):
-                    ftype=h5record.dtmap[data[0].dtype]
-                else:
-                    ftype='f' if isinstance(data[0], float) else 'i'
-                if  hasattr(data[0],'shape'): #numpy array
-                    self.datasets[name] = self.h5file.create_dataset(name, (self.maxidx,) + data[0].shape,ftype,
-                                                                 chunks=(10,) + data[0].shape, compression='lzf')
-                elif hasattr(data[0],'__iter__'): # python list
-                    print(type(data),type(data[0]))
-                    self.datasets[name] = self.h5file.create_dataset(name, (self.maxidx, len(data[0])),ftype,
-                                                               chunks=(10, len(data[0])), compression='lzf')
-                else:
-                    print(type(data),type(data[0])) #scalar
-                    self.datasets[name] = self.h5file.create_dataset(name, (self.maxidx,1),
-                                                                ftype,
-                                                                chunks=(10,1), compression='lzf')
+            try:
+                data = path.get(name,None)
+                #print("name {} len {}".format(name,len(data)))
+                if name in self.exclude:
+                    continue
+                if not batch:
+                    data=[data]
+                if len(data)==0:continue
+                if not name in self.datasets:
+                    assert self.idx==0,'{} data set must appear in first call to record {} '.format(name,self.idx)
+                    if  name in self.h5file.keys():
+                        del self.h5file[name]
+                    if hasattr(data[0],'dtype'):
+                        ftype=h5record.dtmap[data[0].dtype]
+                    else:
+                        ftype='f'
+                    if  hasattr(data[0],'shape'): #numpy array
+                        self.datasets[name] = self.h5file.create_dataset(name, (self.maxidx,) + data[0].shape,ftype,
+                                                                     chunks=(10,) + data[0].shape, compression='lzf')
+                    elif hasattr(data[0],'__iter__'): # python list
+                        print(type(data),type(data[0]))
+                        self.datasets[name] = self.h5file.create_dataset(name, (self.maxidx, len(data[0])),ftype,
+                                                                   chunks=(10, len(data[0])), compression='lzf')
+                    else:
+                        print(type(data),type(data[0])) #scalar
+                        self.datasets[name] = self.h5file.create_dataset(name, (self.maxidx,1),
+                                                                    ftype,
+                                                                    chunks=(10,1), compression='lzf')
 
-            if l is None:
-                l = len(data)
-            assert l == len(data),'{}: all path data must have the same length {} != {}'.format(name,l,len(data))
-            for idx in range(l):
-                self.datasets[name][self.idx+idx]=data[idx]
+                if l is None:
+                    l = len(data)
+                assert l == len(data),'{}: all path data must have the same length {} != {}'.format(name,l,len(data))
+                for idx in range(l):
+                        self.datasets[name][self.idx+idx]=data[idx]
+            except Exception as e:
+                print(name,e)
         self.idx += l
         self.h5file.attrs['maxidx']=self.idx
         self.h5file.flush()
